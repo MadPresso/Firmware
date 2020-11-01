5a8a5f35711d3dea66b33ae536b14c601d2dca55
Daniel Mack <daniel@zonque.org>
2020-11-01 15:27:42 +0100

    pin sync

--- src/pins.h
+++ src/pins.h
@@ -2,15 +2,15 @@
 
 #include <Arduino.h>
 
-#define PIN_VALVE_TRIAC         D1
-#define PIN_PUMP_TRIAC          D2
-#define PIN_HEATER_TRIAC        D3
+#define PIN_VALVE_TRIAC         D0
+#define PIN_PUMP_TRIAC          D5
+#define PIN_HEATER_TRIAC        D6
 
-#define PIN_LED_1               D5
-#define PIN_LED_2               D0
+#define PIN_LED_1               D2
+#define PIN_LED_2               D1
 #define PIN_LED_3               D4
 
-#define PIN_SHOT_SENSOR         D6
+#define PIN_SHOT_SENSOR         D3
 #define PIN_STEAM_SENSOR        D7
 #define PIN_ZC_DETECT           D8
 
