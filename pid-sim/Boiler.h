bcfc5fa19bdb32730da07279ce8f6ab6248c5ca1
Daniel Mack <daniel@zonque.org>
2021-01-31 21:14:33 +0100

    refine pid simulator code

--- pid-sim/Boiler.h
+++ pid-sim/Boiler.h
@@ -1,13 +1,27 @@
-#include "Arduino.h"
-#include "accumulator.h"
-
 #pragma once
 
+#include "Arduino.h"
+#include "accumulator.h"
+#include <cmath>
+
 struct BoilerParameters {
-  float waterVolume; // Liters
-  float boilerWeight; // kg
-  float boilerMaterialHeatCapacity; // KJ/(kg*K)
-  float heaterPower; // W
+  // Volume of the water reservoir in litres
+  float waterVolume;
+  // Weight of the boiler, in kg
+  float boilerWeight;
+  // Specfic heat capacity coefficient of the boiler material, in KJ/(kg*K)
+  float boilerMaterialHeatCapacity;
+  // Power of the heating element, in Watts
+  float heaterPower;
+  // Numeric constants that define the rates at which heat dissipates from the
+  // water reservoir to the boiler, and from the boiler to the rest of the
+  // machine, respectively.
+  float boilerConstant;
+  float machineConstant;
+  // Numeric constants that define the cool-down effect of the boiler and the
+  // machine, respectively.
+  float boilerCoolingFactor;
+  float machineCoolingFactor;
 };
 
 class Boiler
@@ -22,7 +36,6 @@ private:
   float machineTemperature;
 
   Accumulator *heaterAccumulator;
-  Accumulator *boilerAccumulator;
 
 public:
   Boiler(BoilerParameters *params, float startTemperature, float ambientTemperature);
@@ -30,6 +43,6 @@ public:
   void setambientTemperature(float celcius) { ambientTemperature = celcius; }
   void setHeaterPowerPercentage(float perc) { heaterPowerPercentage = perc; }
 
-  float getTemperature() { return boilerTemperature; } // boilerAccumulator->mean();
+  float getTemperature() { return boilerTemperature; }
   float getWaterTemperature() { return waterTemperature; }
 };
--- pid-sim/main.cpp
+++ pid-sim/main.cpp
@@ -7,46 +7,37 @@
 
 unsigned long _millis;
 
-int main(void) {
-  _millis = 0;
-
-  PIDController *pid = new PIDController(0, 255);
-  //pid->setParams(25, 3, 128);
-  pid->setParams(25, 3, 12);
-  pid->setTarget(100.0);
-
   // Values for Ranchilio Siliva
+static struct BoilerParameters param = {
+  .waterVolume = 0.3,
+  .boilerWeight = 1.0,
+  .boilerMaterialHeatCapacity = 0.377, // brass
+  .heaterPower = 1150,
+  .boilerConstant = 0.019,
+  .machineConstant = 0.0025,
+  .boilerCoolingFactor = std::exp(-0.0021),
+  .machineCoolingFactor = std::exp(-0.00014),
+};
 
-  struct BoilerParameters param = {
-    .waterVolume = 0.30,
-    .boilerWeight = 1.0,
-    .boilerMaterialHeatCapacity = 0.377,
-    .heaterPower = 1150, //1150,
-  };
-
-  Boiler *b = new Boiler(&param, 22.18, 19.2);
-
-  std::ifstream infile("../data-1.csv");
+void plotMeasurementFile(const char *fileName, float startTemperature, float ambientTemperature) {
+  std::ifstream infile(fileName);
 
   int dHeater;
   float dTemp;
   int t = 0;
 
-  // int skip = 180;
+  Boiler *b = new Boiler(&param, startTemperature, ambientTemperature);
 
-  // b->setTemperature(101.3);
+  // int skip = 180;
 
   while (infile >> dHeater >> dTemp) {
     // if (--skip > 0)
     //   continue;
 
     b->tick();
-
-//    int v = pid->compute(t);
-//    b->setHeater((float) v);
     b->setHeaterPowerPercentage(float(dHeater)/2.55);
-   float sTemp = b->getTemperature();
-    printf("%d %.2f %.2f %d %.2f\n", t, sTemp, dTemp, dHeater, b->getWaterTemperature());
+    float sTemp = b->getTemperature();
+    printf("%d %.2f %.2f %d\n", t, sTemp, dTemp, dHeater);
     // printf("%d %.2f %d\n", t, dTemp, dHeater);
     // if (t % 1000 == 0)
     //   printf("%d %.2f\n", t, dTemp);
@@ -54,9 +45,50 @@ int main(void) {
     millis_tick(100);
     t++;
 
-    if (t > 10000)
+    if (t > 2000)
       break;
   }
+}
+
+void plotPID() {
+  PIDController *pid = new PIDController(0, 255);
+  pid->setParams(25, 3, 12);
+  pid->setTarget(100.0);
+
+  Boiler *b = new Boiler(&param, 18.0, 18.0);
+
+  for (int t = 0; t < 10000; t++) {
+    b->tick();
+
+    if (t == 2000)
+      pid->setTarget(110);
+
+    if (t == 4000)
+      pid->setTarget(90);
+
+    if (t == 6000)
+      pid->setTarget(120);
+
+    if (t == 8000)
+      pid->setTarget(80);
+
+    float sTemp = b->getTemperature();
+    int v = pid->compute(sTemp);
+
+    b->setHeaterPowerPercentage(((float) v)/2.55);
+
+    printf("%d %.2f %.2f %d\n", t, sTemp, pid->currentTarget(), v);
+
+    millis_tick(1000);
+  }
+}
+
+int main(void) {
+  _millis = 0;
+
+  //plotMeasurementFile("data-1.csv", 22.18, 19.2);
+  //plotMeasurementFile("data-on-off-heating.csv", 17.4, 17.4);
+  plotPID();
 
   return 0;
 }
